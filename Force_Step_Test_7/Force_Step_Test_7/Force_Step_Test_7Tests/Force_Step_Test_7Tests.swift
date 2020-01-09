7bbf049308136ced2c59fd12ef18bcd28076b694
OwnMacBookPro <vkqxkr@gmail.com>
2020-01-09 12:20:24 +0900

    Initial commit

---
+++ Force_Step_Test_7/Force_Step_Test_7/Force_Step_Test_7Tests/Force_Step_Test_7Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Force_Step_Test_7Tests.swift
+//  Force_Step_Test_7Tests
+//
+//  Created by JinHyuck Park on 2020/01/09.
+//  Copyright © 2020 JinHyuck Park. All rights reserved.
+//
+
+import XCTest
+@testable import Force_Step_Test_7
+
+class Force_Step_Test_7Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
